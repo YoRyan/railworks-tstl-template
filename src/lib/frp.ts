3fbb2dbae833f3f7a3a098e4ed6f991943403a50
Ryan Young <ryan@youngryan.com>
2022-05-18 21:10:36 +0000

    feat: initial commit

---
+++ src/lib/frp.ts
@@ -0,0 +1,175 @@
+/** @noSelfInFile */
+/**
+ * Functional reactive programming library.
+ * Straight port of https://github.com/santoshrajan/frpjs to TypeScript.
+ */
+
+import * as rw from "./railworks";
+
+export type Stream<T> = (next: (value: T) => void) => void;
+
+/**
+ * Takes an eventStream and a function that transforms the value of the Event.
+ * Returns a new Event that emits the transformed Value
+ */
+export function map<T, U>(valueTransform: (value: T) => U): (eventStream: Stream<T>) => Stream<U> {
+    return function (eventStream) {
+        return function (next) {
+            eventStream(function (value) {
+                next(valueTransform(value));
+            });
+        };
+    };
+}
+
+/**
+ * Binds an eventStream to a new EventStream. Function valueToEvent is called
+ * with the event value. Returns a new Event Stream.
+ */
+export function bind<T, U>(valueToEvent: (value: T) => Stream<U>): (eventStream: Stream<T>) => Stream<U> {
+    return function (eventStream) {
+        return function (next) {
+            eventStream(function (value) {
+                valueToEvent(value)(next);
+            });
+        };
+    };
+}
+
+/**
+ * Filters an Event Stream. Predicate is called with every value.
+ */
+export function filter<T>(predicate: (value: T) => boolean): (eventStream: Stream<T>) => Stream<T> {
+    return function (eventStream) {
+        return function (next) {
+            eventStream(function (value) {
+                if (predicate(value)) {
+                    next(value);
+                }
+            });
+        };
+    };
+}
+
+/**
+ * Opposite of filter
+ */
+export function reject<T>(predicate: (value: T) => boolean): (eventStream: Stream<T>) => Stream<T> {
+    return function (eventStream) {
+        return function (next) {
+            eventStream(function (value) {
+                if (!predicate(value)) {
+                    next(value);
+                }
+            });
+        };
+    };
+}
+
+/**
+ * Is the 'reduce' function for every event in the stream. The step function
+ * is called with the accumulator and the current value. The parameter initial
+ * is the initial value of the accumulator
+ */
+export function fold<T>(step: (accumulated: T, value: T) => T, initial: T): (eventStream: Stream<T>) => Stream<T> {
+    return function (eventStream) {
+        return function (next) {
+            let accumulated = initial;
+            eventStream(function (value) {
+                next((accumulated = step(accumulated, value)));
+            });
+        };
+    };
+}
+
+/**
+ * Takes two eventStreams, combines them and returns a new eventStream
+ */
+export function merge<A, B>(eventStreamA: Stream<A>): (eventStreamB: Stream<B>) => Stream<A | B> {
+    return function (eventStreamB) {
+        return function (next) {
+            eventStreamA(value => next(value));
+            eventStreamB(value => next(value));
+        };
+    };
+}
+
+/**
+ * Takes an eventStream, performs a series of operations on it and returns
+ * a modified stream. All FRP operations are curried by default.
+ */
+export function compose(eventStream: Stream<any>, ...operations: ((eventStream: Stream<any>) => Stream<any>)[]) {
+    if (operations.length === 0) {
+        return eventStream;
+    }
+
+    let operation = operations.shift();
+    return compose(operation(eventStream), ...operations);
+}
+
+/**
+ * Returns a behaviour. Call the behaviour for the last value of the event.
+ */
+export function stepper<T>(eventStream: Stream<T>, initial: T): () => T {
+    let valueAtLastStep = initial;
+
+    eventStream(function nextStep(value) {
+        valueAtLastStep = value;
+    });
+
+    return function behaveAtLastStep() {
+        return valueAtLastStep;
+    };
+}
+
+/**
+ * Throttle an EventStream to every ms milliseconds
+ *
+ * @description Note that unlike Santosh Rajan's original version of the
+ * function, this one is curried.
+ */
+export function throttle<T>(ms: number): (eventStream: Stream<T>) => Stream<T> {
+    return function (eventStream) {
+        return function (next) {
+            let last = 0;
+            eventStream(function (value) {
+                let now = rw.o.GetSimulationTime() / 1000;
+                if (last === 0 || now - last > ms) {
+                    next(value);
+                    last = now;
+                }
+            });
+        };
+    };
+}
+
+export function snapshot<T>(behavior: (() => T) | T): T {
+    if (behavior instanceof Function) {
+        return behavior();
+    }
+    return behavior;
+}
+
+export function liftN<T>(combine: (...args: any[]) => T, ...behaviors: any[]): () => T {
+    return function () {
+        let values = behaviors.map(value => snapshot(value));
+        return combine(...values);
+    };
+}
+
+export function hub<T>(): (eventStream: Stream<T>) => Stream<T> {
+    return function (eventStream) {
+        let nexts = [];
+        let isStarted = false;
+
+        return function (next) {
+            nexts.push(next);
+            if (!isStarted) {
+                eventStream(function (value) {
+                    nexts.forEach(next => next(value));
+                });
+                isStarted = true;
+            }
+        };
+    };
+}
